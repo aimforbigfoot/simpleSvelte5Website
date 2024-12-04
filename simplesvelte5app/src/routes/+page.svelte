394ad0899d99b8a76283d6dec78237fafb5710ed
NAD LAB <aimforbigfoot@gmail.com>
2024-12-03 19:51:12 -0500

    end of video commit
    BIG STUFF DONE TODAY

--- simplesvelte5app/src/routes/+page.svelte
+++ simplesvelte5app/src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script>
+	let asdfasdfasdf = $state(0);
+	let b = 4;
+    let randomTextIType = $state("asdfasdf")
+    
+
+
+    const whenBtnIsBlicked = (a) => {
+        console.log("smth was pressed")
+        asdfasdfasdf += 1
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-sdfsdfsdfsdfdfs dfsd fsd
+
+<button onclick={whenBtnIsBlicked}>text</button>
+{asdfasdfasdf}
+
+<input type="text" bind:value={randomTextIType} />
+{randomTextIType}
